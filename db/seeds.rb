fd4251a3137f441f2b56812943780c7225d2fd47
anton_semenichenko <asemenichenko.codica@gmail.com>
2018-02-13 20:27:45 +0200

    backend init commit

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Item.create!([
+   { name: 'Computer', description: 'Apple iMac OVER9000', price: 30 },
+   { name: 'Laptop', description: 'Lenovo Laptop', price: 11 },
+   { name: 'Music Record', description: 'Jack White Best Compilation', price: 2 },
+])
